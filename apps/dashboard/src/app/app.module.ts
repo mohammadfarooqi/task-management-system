b1914858f6abb68a157c9cb59f0d133a2411f01a
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2025-08-24 12:53:32 -0400

    code clean up

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { App } from './app';
+import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcome } from './nx-welcome';
 
 @NgModule({
-  declarations: [App, NxWelcome],
+  declarations: [AppComponent],
   imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [App],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
