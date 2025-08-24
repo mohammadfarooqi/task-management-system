cc0dd8b5270ac99764027afa456a05089f176759
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2025-08-24 09:44:56 -0400

    fixed: update base endpoint to be public to test api running

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from '@task-management-system/auth';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getData() {
     return this.appService.getData();
